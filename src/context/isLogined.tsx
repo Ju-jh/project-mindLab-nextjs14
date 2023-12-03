54347d9c184e9b7be1ff4d2595411b444fd3c6d0
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-12-03 17:42:36 +0900

    header accessToken

---
+++ src/context/isLogined.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import axios from "axios";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+
+interface AuthContextProps {
+  accessToken: boolean;
+}
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [accessToken, setAccessToken] = useState<boolean>(false);
+
+  useEffect(() => {
+    axios
+      .post("https://port-0-coin-market-be-12fhqa2llob5p0if.sel5.cloudtype.app/user/cookie")
+      .then((response) => {
+        if (response.data && response.data.isCookie) {
+          setAccessToken(true);
+        } else {
+          setAccessToken(false);
+        }
+      })
+      .catch(() => {
+        setAccessToken(false);
+      });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ accessToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = (): AuthContextProps => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
