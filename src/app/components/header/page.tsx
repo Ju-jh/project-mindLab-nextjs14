56bfc13e44b8455e3829f17bca9609f257c60cbe
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-12-03 17:29:56 +0900

    header complete.

--- src/app/components/header/page.tsx
+++ src/app/components/header/page.tsx
@@ -7,10 +7,24 @@ export default function Header() {
 
   return (
     <header className=' fixed w-full h-[60px] shadow-md bg-white z-50'>
-      <div className='h-full flex flex-col items-start justify-center pl-[50px]'>
+      <div className='h-full flex items-start justify-between px-[50px]'>
         <Link href={'/'}>
           <Image src={'/icon.png'} alt='IconImage' width={100} height={100}></Image>
         </Link>
+      { false && <Link href={'https://mind-lab-be-bffdf1dcb8ba.herokuapp.com/user/google/callback'}>
+          <button className='flex items-center justify-center  h-[50px] m-[5px] shadow-md rounded-md  p-[20px]'>
+            <Image src={'/google.png'} alt='GoogleImage' width={30} height={30}></Image>
+            <span className='font-bold ml-[5px]'>Google Login</span>
+          </button>
+        </Link>}
+        {true &&
+          <div className='flex items-center justify-center  h-[50px] m-[5px] p-[20px]'>
+            <div className='w-[40px] h-[40px] overflow-hidden rounded-full shadow-md bg-slate-200'>
+              <Image src={'/photo.png'} alt='ProfileImage' width={40} height={40}></Image>
+            </div>
+            <span className='font-bold ml-[20px]'>email@gmail.com</span>
+          </div>
+        }
       </div>
     </header>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <main className='main flex-col w-full min-h-[1400px] p-[30px] pt-[90px]'>
       <div className='cardsDiv flex items-start justify-start flex-wrap gap-[30px] w-full h-[700px] shadow-sm shadow-slate-400 rounded-md p-[30px]'>
         <div className='w-[350px] h-[300px]'>
-          <Link href={'/1'}>
+          <Link href={'/survey'}>
             <button
               className='buttonDiv flex flex-col items-center justify-center w-full h-full rounded-lg shadow-md shadow-slate-400 text-[22px] hover:text-[24px] hover:bg-slate-300 transition-all'
             >
