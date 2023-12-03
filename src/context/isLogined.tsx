94fa7e990b38f24608bdfa5e2a4af40100e9f7af
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-12-03 19:24:07 +0900

    axios to apollo

--- src/context/isLogined.tsx
+++ src/context/isLogined.tsx
@@ -1,39 +1,25 @@
-'use client'
-
-import axios from "axios";
-import React, {
-  createContext,
-  useContext,
-  useEffect,
-  useState,
-  ReactNode,
-} from "react";
+import { gql, useQuery } from '@apollo/client';
+import React, { createContext, useContext, ReactNode } from 'react';
 
 interface AuthContextProps {
   accessToken: boolean;
 }
+
+const CHECK_AUTH = gql`
+  query {
+    checkAuth {
+      isCookie
+    }
+  }
+`;
 
 const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const [accessToken, setAccessToken] = useState<boolean>(false);
-
-  useEffect(() => {
-    axios
-      .post("https://mind-lab-be-bffdf1dcb8ba.herokuapp.com/user/cookie")
-      .then((response) => {
-        if (response.data && response.data.isCookie) {
-          setAccessToken(true);
-        } else {
-          setAccessToken(false);
-        }
-      })
-      .catch(() => {
-        setAccessToken(false);
-      });
-  }, []);
+  const { data, loading, error } = useQuery(CHECK_AUTH);
+  const accessToken = data?.checkAuth?.isCookie || false;
 
   return (
     <AuthContext.Provider value={{ accessToken }}>
@@ -45,7 +31,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({
 export const useAuth = (): AuthContextProps => {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error("useAuth must be used within an AuthProvider");
+    throw new Error('useAuth must be used within an AuthProvider');
   }
   return context;
 };
