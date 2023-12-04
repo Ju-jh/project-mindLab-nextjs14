69f1402b385164fa59b08d36d4eabd730068fe52
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-12-04 12:34:37 +0900

    Login part dev complete

--- src/app/components/header/page.tsx
+++ src/app/components/header/page.tsx
@@ -6,11 +6,8 @@ import Link from 'next/link';
 export default function Header() {
 
   const { accessToken } = useAuth();
-  console.log(accessToken,'여기가 accessToken')
-
 
   const redirectAfterLogoutPath = () => {
-    accessToken === false;
     window.location.href = "https://mind-lab-be-bffdf1dcb8ba.herokuapp.com/user/logout";
   };
 
