a7d0193ea316960714385295f0202f862e8848fb
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-12-05 17:55:57 +0900

    page id

--- src/graphql/Get/query.ts
+++ src/graphql/Get/query.ts
@@ -1,20 +1,21 @@
+import axios from 'axios';
+
 interface GraphQLQuery {
   query: string;
 }
 
-export async function getGraphQLQuery({query}: GraphQLQuery) {
+export async function getGraphQLQuery({ query }: GraphQLQuery) {
   const endpoint = 'https://mind-lab-be-bffdf1dcb8ba.herokuapp.com/graphql';
 
   try {
-    const response = await fetch(`${endpoint}?query=${encodeURIComponent(query)}`, {
-      method: 'GET',
+    const response = await axios.get(endpoint, {
+      params: { query },
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
-    const data = await response.json();
-    return data;
+    return response.data;
   } catch (error) {
     console.error('GraphQL 쿼리 실패:', error);
     throw error;
