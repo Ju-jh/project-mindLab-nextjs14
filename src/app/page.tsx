73b71968f7a4a92c14d4606f808f110f9036726f
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-12-05 16:16:31 +0900

    get My Surveys

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { faX } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
+import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode } from 'react';
 
 async function sendGraphQLQuery(query: any, variables: any) {
   const endpoint = 'https://mind-lab-be-bffdf1dcb8ba.herokuapp.com/graphql';
@@ -24,8 +25,14 @@ async function sendGraphQLQuery(query: any, variables: any) {
   }
 }
 
+interface HomeProps {
+  mySurveys: Array<{
+    s_id: string;
+    title: string;
+  }>;
+}
 
-export default function Home() {
+export default function Home({ mySurveys }:HomeProps) {
 
   async function createSurvey() {
     const query = `
@@ -50,18 +57,20 @@ export default function Home() {
         <span className='font-bold text-[22px]'>내가 만든 설문지</span>
       </div>
       <div className='cardsDiv flex items-start justify-start flex-wrap gap-[30px] w-full h-[300px] shadow-sm shadow-slate-400 rounded-md p-[30px] mt-[20px]'>
-        <div className='w-[250px] h-[150px]'>
-          <Link href={'/mysurvey'}>
-            <button
-              className='buttonDiv flex flex-col items-center justify-center w-full h-full rounded-lg shadow-md shadow-slate-400 text-[16px] hover:text-[18px] hover:bg-slate-300 transition-all'
-            >
-              <span className=''>내가만든 설문지 제목</span>
+        {mySurveys.map((survey) => (
+          <div key={survey.s_id} className='w-[250px] h-[150px]'>
+            <Link href={`/mysurvey/${survey.s_id}`}>
+              <button
+                className='buttonDiv flex flex-col items-center justify-center w-full h-full rounded-lg shadow-md shadow-slate-400 text-[16px] hover:text-[18px] hover:bg-slate-300 transition-all'
+              >
+                <span>{survey.title}</span>
+              </button>
+            </Link>
+            <button className='absolute flex flex-col items-center justify-center translate-x-[235px] translate-y-[-160px]  w-[30px] h-[30px] rounded-full bg-green-300 hover:bg-green-500'>
+              <FontAwesomeIcon icon={faX} className='w-[40%]'/>
             </button>
-          </Link>
-          <button className='absolute flex flex-col items-center justify-center translate-x-[235px] translate-y-[-160px]  w-[30px] h-[30px] rounded-full bg-green-300 hover:bg-green-500'>
-            <FontAwesomeIcon icon={faX} className='w-[40%]'/>
-          </button>
-        </div>
+          </div>
+        ))}
         <button
           onClick={createSurvey}
           className='newCardDiv flex flex-col items-center justify-center w-[250px] h-[150px] rounded-lg shadow-md shadow-slate-400 text-[55px] hover:text-[70px] hover:bg-slate-300 transition-all'>
@@ -85,3 +94,24 @@ export default function Home() {
     </main>
   );
 }
+
+export async function getServerSideProps() {
+  const query = `
+    query GetMySurvey {
+      getMySurvey {
+        s_id
+        title
+      }
+    }
+  `;
+
+  try {
+    const result = await sendGraphQLQuery(query, {});
+    const mySurveys = result.data.getMySurvey || [];
+    return { props: { mySurveys } };
+  } catch (error) {
+    console.error('Error while getting my surveys:', error);
+    return { props: { mySurveys: [] } };
+  }
+}
+
