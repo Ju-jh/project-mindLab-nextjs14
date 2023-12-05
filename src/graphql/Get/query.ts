7e5513171fd03dc7c5bd85cd71255e763aeaef52
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-12-05 17:59:52 +0900

    fix

--- src/graphql/Get/query.ts
+++ src/graphql/Get/query.ts
@@ -1,21 +1,20 @@
-import axios from 'axios';
-
 interface GraphQLQuery {
   query: string;
 }
 
-export async function getGraphQLQuery({ query }: GraphQLQuery) {
+export async function getGraphQLQuery({query}: GraphQLQuery) {
   const endpoint = 'https://mind-lab-be-bffdf1dcb8ba.herokuapp.com/graphql';
 
   try {
-    const response = await axios.get(endpoint, {
-      params: { query },
+    const response = await fetch(`${endpoint}?query=${encodeURIComponent(query)}`, {
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
-    return response.data;
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error('GraphQL 쿼리 실패:', error);
     throw error;
