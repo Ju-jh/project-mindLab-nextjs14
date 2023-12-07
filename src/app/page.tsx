4a7192314b94f61d7058f2e47c64c5ca544ab272
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-12-07 16:49:05 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
   const { accessToken, email, photo } = useAuth();
 
   const [mySurveys, setMySurveys] = useState<Survey[]>([]);
+  const [publicSurveys, setPublicSurveys] = useState<Survey[]>([]);
 
   const getMySurvey = async () => {
       const query = `
@@ -77,8 +78,27 @@ export default function Home() {
     }
   };
 
+  const getPublicSurvey = async () => {
+      const query = `
+        query GetPublicSurvey {
+          getPublicSurvey {
+            s_id
+            title
+          }
+        }
+      `;
+      try {
+        const result = await getGraphQLQuery({ query });
+        const publicSurveysData = result.data.getPublicSurvey || [];
+        setPublicSurveys(publicSurveysData);
+      } catch (error) {
+        console.error('나의 설문지 가져오기 실패:', error);
+      }
+  };
+
   useEffect(() => {
     getMySurvey();
+    getPublicSurvey();
   },[])
 
   return (
@@ -108,6 +128,7 @@ export default function Home() {
             </button>
           </div>
         ))}
+        
         <button
           onClick={createSurvey}
           className='newCardDiv flex flex-col items-center justify-center w-[250px] h-[150px] rounded-lg shadow-md shadow-slate-400 text-[55px] hover:text-[70px] hover:bg-slate-300 transition-all'>
@@ -119,15 +140,22 @@ export default function Home() {
         <span className='font-bold text-[22px]'>참여하는 설문지</span>
       </div>
       <div className='cardsDiv flex items-start justify-start flex-wrap gap-[30px] w-full h-[300px] shadow-sm shadow-slate-400 rounded-md p-[30px] mt-[20px]'>
-        <div className='w-[250px] h-[150px]'>
-          <Link href={'/pushedsurvey/'}>
+        { publicSurveys.map((survey) => (
+          <div key={survey.s_id} className='w-[250px] h-[150px]'>
+            <Link href={`/pusedsurvey/${survey.s_id}`}>
+              <button
+                className='buttonDiv flex flex-col items-center justify-center w-full h-full rounded-lg shadow-md shadow-slate-400 text-[16px] hover:text-[18px] hover:bg-slate-300 transition-all'
+              >
+                <span>{survey.title}</span>
+              </button>
+            </Link>
             <button
-              className='buttonDiv flex flex-col items-center justify-center w-full h-full rounded-lg shadow-md shadow-slate-400 text-[16px] hover:text-[18px] hover:bg-slate-300 transition-all'
-            >
-              <span className=''>참여하는 설문지 제목</span>
+              onClick={() => deleteSurvey(survey.s_id)}
+              className='absolute flex flex-col items-center justify-center translate-x-[235px] translate-y-[-160px]  w-[30px] h-[30px] rounded-full bg-green-300 hover:bg-green-500'>
+              <FontAwesomeIcon icon={faX} className='w-[40%]'/>
             </button>
-          </Link>
-        </div>
+          </div>
+        ))}
       </div>
     </main>
   );
